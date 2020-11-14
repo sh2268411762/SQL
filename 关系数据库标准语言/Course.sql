56350cd80334d203bccefa837213956a63f1f93c
sh2268411762 <2268411762@qq.com>
2020-11-14 21:48:05 +0800

    数据库操作

--- 关系数据库标准语言/Course.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE Course
-(
-	Cno CHAR(4) PRIMARY KEY, /*列级完整性的约束条件，Cno是主码*/
-	Cname CHAR(40) NOT NULL, /*列级完整性约束条件，Cname不能取空值*/
-	Cpno CHAR(4),            /*Cpno 的含义是先修课*/
-	Ccredit SMALLINT,
-	FOREIGN KEY (Cno) REFERENCES Course(Cno)
-);
